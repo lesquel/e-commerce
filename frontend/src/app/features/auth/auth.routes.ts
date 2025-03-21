bd2d303079ac04b0e3d6ee2fad4bd17ffc03074e
lesquel <lesquel1319@gmail.com>
2025-03-21 16:51:56 -0500

    routes auth

--- frontend/src/app/features/auth/auth.routes.ts
+++ frontend/src/app/features/auth/auth.routes.ts
@@ -1,29 +1,32 @@
-import { Routes } from "@angular/router";
-import { AuthLayout } from "./auth.layout";
-import { EditPage, LoginPage, RegisterPage } from "./pages";
-import { MePage } from "./pages";
+import { Routes } from '@angular/router';
+import { AuthLayout } from './auth.layout';
+import { EditPage, LoginPage, RegisterPage } from './pages';
+import { MePage } from './pages';
+import { routesConfig } from './config';
 
 export const authRoutes: Routes = [
-    {
-        path: 'auth',
-         component: AuthLayout,
+  {
+    path: routesConfig.auth.path,
+    component: AuthLayout,
+    children: [
+      {
+        path: routesConfig.login.path,
+        component: LoginPage,
+      },
+      {
+        path: routesConfig.register.path,
+        component: RegisterPage,
+      },
+      {
+        path: routesConfig.me.path,
+        component: MePage,
         children: [
-            {
-                path: 'login',
-                component: LoginPage
-            },
-            {
-                path: 'register',
-                component: RegisterPage
-            },
-            {
-                path: 'me',
-                component: MePage
-            },
-            {
-                path: 'me/edit',
-                component: EditPage
-            }
-        ]
-    },
+          {
+            path: routesConfig.edit.path,
+            component: EditPage,
+          },
+        ],
+      },
+    ],
+  },
 ];
--- frontend/src/app/features/auth/config/routes.config.ts
+++ frontend/src/app/features/auth/config/routes.config.ts
@@ -1,22 +1,22 @@
 export const routesConfig = {
   auth: {
-    path: '/auth',
+    path: 'auth',
     url: '/auth',
   },
   login: {
-    path: '/login',
+    path: 'login',
     url: '/auth/login',
   },
   register: {
-    path: '/register',
+    path: 'register',
     url: '/auth/register',
   },
   me: {
-    path: '/me',
+    path: 'me',
     url: '/auth/me',
   },
   edit: {
-    path: '/edit',
+    path: 'edit',
     url: '/auth/me/edit',
   },
 };
