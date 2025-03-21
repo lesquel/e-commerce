398e015e75af3b02272ab7d28bdbc6352253c5d0
lesquel <lesquel1319@gmail.com>
2025-03-21 15:34:47 -0500

    home

---
+++ frontend/src/app/feature/site/pages/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HeroHomeComponent } from '../components/home/hero-home/hero-home.component';
+import { ContentChampionshipsComponent } from '../../championship/components/';
+import { ContentTeamComponent } from '../../teams/components';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'home-page',
+  imports: [
+    HeroHomeComponent,
+    ContentChampionshipsComponent,
+    ContentTeamComponent,
+    RouterLink,
+  ],
+  template: `
+    <app-hero-home />
+    <app-content-championships [pageSize]="10" />
+    <app-content-team [pageSize]="10" />
+    <a [routerLink]="['/teams']" routerLinkActive="router-link-active">
+  `,
+})
+export class HomePage {}
