3089127a933829194f73bfb9457ccc18b148ca1f
lesquel <lesquel1319@gmail.com>
2025-03-27 18:37:12 -0500

    nose que paso

--- frontend/src/app/features/shopping-cart/components/slide-cart-content/slide-cart-content.component.ts
+++ frontend/src/app/features/shopping-cart/components/slide-cart-content/slide-cart-content.component.ts
@@ -1,11 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { UserService } from '@app/features/auth/services';
+import { ButtonIsLoginComponent } from "../../../auth/components/button-is-login/button-is-login.component";
 
 @Component({
   selector: 'app-slide-cart-content',
-  imports: [],
+  imports: [ButtonIsLoginComponent],
   templateUrl: './slide-cart-content.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SlideCartContentComponent {
-
+  private userService = inject(UserService);
+  protected user = this.userService.isAuthenticated();
 }
