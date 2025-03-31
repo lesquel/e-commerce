5ff6630b7cf92b09d2515bc4df841a00df04c620
lesquel <lesquel1319@gmail.com>
2025-03-31 11:14:10 -0500

    notify

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,35 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
 import { FooterComponent } from '@shared/components/footer/footer.component';
-import { NotificationComponent } from "./shared/components/notification/notification.component";
-import { NotificationsService } from './shared/services/notifications.service';
-import { NotificationType } from './shared/types';
-
+import { NotificationComponent } from './shared/components/notification/notification.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, NotificationComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    NotificationComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  notificationsService = inject(NotificationsService)
-
   title = 'E-commerce';
-  showAlert: Boolean = false;
-  message: string = '';
-  notificationType: NotificationType = NotificationType.AlertError;
-
-  ngOnInit() {
-    this.notificationsService.alert$.subscribe((res: any) => {
-      this.showAlert = true;
-      this.message = res.message;
-      this.notificationType = res.notificationType;
-      setTimeout(() => {
-        this.showAlert = false
-      }, res.time);
-    })
-  }
-
 }
--- frontend/src/app/shared/components/notification/notification.component.ts
+++ frontend/src/app/shared/components/notification/notification.component.ts
@@ -1,16 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { NotificationsService } from '@app/shared/services/notifications.service';
 import { NotificationType } from '@app/shared/types';
 
 @Component({
   selector: 'app-notification',
   imports: [CommonModule, MatIconModule],
   templateUrl: './notification.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotificationComponent {
-  showNotification = input.required<Boolean>()
-  notificationMessage = input<String>()
-  notificationType = input.required<NotificationType>()
+  notificationsService = inject(NotificationsService);
+  notificationType: NotificationType = NotificationType.AlertError;
+  message = signal('');
+  showAlert = signal(false);
+
+  ngOnInit() {
+    this.notificationsService.alert$.subscribe((res: any) => {
+      this.showAlert.set(true);
+      this.message.set(res.message);
+      this.notificationType = res.notificationType;
+      setTimeout(() => {
+        this.showAlert.set(false);
+      }, res.time);
+    });
+  }
 }
